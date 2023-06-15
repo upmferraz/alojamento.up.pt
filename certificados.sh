cabff05560af493e8575776d73630876969ad18f
mferraz <mferraz@uporto.pt>
2023-06-15 12:22:34 +0100

    Add files via upload

--- certificados.sh
+++ certificados.sh
@@ -2,12 +2,15 @@
 # Define o endereço do servidor de testes
 SERVER="web.up.pt"
 
+# Define a localização dos scripts
+SCRIPTDIR="$HOME/scripts/certificados"
+
 # Tenta conectar ao servidor na porta 22
 nc -z -w5 $SERVER 22 >/dev/null 2>&1
 
 # Verifica o status de saída do comando anterior
 if [ $? -eq 0 ]; then
-	cd $HOME/scripts/certificados
+	cd $SCRIPTDIR
 	python3 get-certificates-from-mail.py
 	./downloads-instala-certificados-https.sh
 else
